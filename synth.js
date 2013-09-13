2bbb0c820fd22f0248c14797adfb9117aa11b1aa
Peter Schroedl <peter_schroedl@me.com>
2013-09-13 15:31:13 -0700

    adds LFO to filter

--- synth.js
+++ synth.js
@@ -14,15 +14,14 @@ $(document).ready(function(){
   var soundSource = audioContext.createBufferSource();
 
   //load sample file over http to our buffer
-  var ajaxReq = new XMLHttpRequest(); 
-  ajaxReq.open("GET","pzBeat.mp3",true);
-  ajaxReq.responseType = "arraybuffer";
-  ajaxReq.onload = function(){
-    var soundBuffer = audioContext.createBuffer(ajaxReq.response,false) //flag is set for stereo
+  var sampleReq = new XMLHttpRequest(); 
+  sampleReq.open("GET","pzBeat.mp3",true);
+  sampleReq.responseType = "arraybuffer";
+  sampleReq.onload = function(){
+    var soundBuffer = audioContext.createBuffer(sampleReq.response,false) //flag is set for stereo
     soundSource.buffer = soundBuffer;
     soundSource.loop = true;
     soundSource.noteOn(0);
-    debugger;
   };
 
   //create/connect lowpass filter
@@ -30,18 +29,24 @@ $(document).ready(function(){
   filterNode.type = 0;
   filterNode.frequency.value = 1000;
 
-  //attach filter into the chain
+  //create Low frequency oscillator
+  var lfoNode = audioContext.createOscillator();
+  lfoNode.type = "sine";
+  lfoNode.frequency = 300;
+  lfoNode.connect(filterNode);
+  //attach filter and reverb into the chain
   soundSource.connect(filterNode);
   filterNode.connect(audioContext.destination);
 
   //play output
-  ajaxReq.send();
-
+  sampleReq.send();
+  
   //dat.gui provides sliders for parameters
   var gui = new dat.GUI();
   this.freq = filterNode.frequency.value;
   this.Q = filterNode.Q.value;
   this.loopEnd = soundSource.loopEnd;
+  //this.LFORate = lfoNode.value;
 
   gui.add(this, 'freq').min(0).max(20000).onChange(function(newVal){
     filterNode.frequency.value = newVal;
@@ -52,8 +57,12 @@ $(document).ready(function(){
   gui.add(this, 'loopEnd').min(0).max(4).onChange(function(newVal){
     soundSource.loopEnd = newVal;
   });
+  // gui.add(this, 'LFORate').min(0).max(100).onChange(function(newVal){
+  //   soundSource.loopEnd = newVal;
+  // });
 
   var leapControlledFrequency = 200;
+  var leapControlledLFORate = 10;
   var leapControlledResonance = 0;
 
   Leap.loop(function(frame) {
@@ -65,11 +74,14 @@ $(document).ready(function(){
       leapControlledFrequency = frame.data.hands[0].palmPosition[1];
     }
     if (frame.hands.length === 2){
-      leapControlledResonance = frame.data.hands[1].palmPosition[1];
+      //leapControlledResonance = frame.data.hands[1].palmPosition[1];
+      
     }
 
-    filterNode.frequency.value = leapControlledFrequency*10;
-    filterNode.Q.value = leapControlledResonance/20;
+
+    filterNode.frequency.value = leapControlledFrequency*10;  //change to generic param 1, param 2
+    
+    //filterNode.Q.value = leapControlledResonance/20;
   });  
 
 
