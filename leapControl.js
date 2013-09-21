e8c00748d031f5a9b2835b1f2efe329c1af4661d
Peter Schroedl <peter_schroedl@me.com>
2013-09-20 20:48:07 -0700

    Includes three.js routines

--- leapControl.js
+++ leapControl.js
@@ -3,10 +3,44 @@ var leapControl = function(sampler){
   var leapControlled = {};
   leapControlled.Frequency = 200;
   leapControlled.LFORate = 10;
-  leapControlled.LFOGain = 100;
+  leapControlled.LFOGain = 80;
   leapControlled.Resonance = 0;
 
+  var scene = new THREE.Scene();
+  var renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  var geometry = new THREE.CubeGeometry( 200, 200, 200 );
+
+  for ( var i = 0; i < geometry.faces.length; i ++ ) {
+    geometry.faces[ i ].color.setHex( Math.random() * 0xffffff );
+  }
+
+  var material = new THREE.MeshBasicMaterial( { vertexColors: THREE.FaceColors } );
+   
+  cube = new THREE.Mesh( geometry, material );
+
+  debugger;
+
+  // cube.position.y = 50;
+  // cube.position.z = 50; 
+  // cube.position.x = 50;
+
+  cube.position.y = this.outerHeight/2;
+  cube.position.z = 50; 
+  cube.position.x = this.outerWidth/2;
+  scene.add(cube);
+
+  var camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
+  camera.position.x = 0;
+  camera.position.y = 150;
+  camera.position.z = 500;
+
+  renderer.render(scene, camera);
 /* Main leap control loop */
+
+
 
   Leap.loop(function(frame) {
     if (frame.hands.length < 1){
@@ -29,6 +63,11 @@ var leapControl = function(sampler){
     sampler.lfoNode.frequency.value = Math.abs(leapControlled.LFORate/10);
     sampler.filterNode.Q.value = leapControlled.Resonance/10;
 
+    cube.position.x = leapControlled.Resonance;
+    cube.position.y = leapControlled.Frequency;
+    cube.position.z = leapControlled.LFORate; 
+
     leapConsole(leapControlled);
+    renderer.render(scene, camera);
   });
 };
