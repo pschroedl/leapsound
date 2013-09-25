3ec259d9d22efae70085df90c824a9a2edac0117
Peter Schroedl <peter_schroedl@me.com>
2013-09-24 17:02:59 -0700

    adds filter2 parameters

--- leapConsole.js
+++ leapConsole.js
@@ -1,9 +1,13 @@
 var leapConsole = function(leapControlled){
     console.clear();
-    console.log("Filter Frequency : " 
-      + leapControlled.Frequency);
-    console.log("Filter Resonance : " 
-      + leapControlled.Resonance);
+    console.log("Filter Frequency1 : " 
+      + leapControlled.Frequency1);
+    console.log("Filter Resonance1 : " 
+      + leapControlled.Resonance1);
+    console.log("Filter Frequency2 : " 
+      + leapControlled.Frequency2);
+    console.log("Filter Resonance2 : " 
+      + leapControlled.Resonance2);
     console.log("LFO Rate : " 
       + leapControlled.LFORate);
     console.log("LFO Rate : " 
