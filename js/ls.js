a3cc485881e81e2136a5f6f38b3f5e6586e5f22a
Peter Schroedl <peter_schroedl@me.com>
2013-09-25 20:14:45 -0700

    comments

--- js/ls.js
+++ js/ls.js
@@ -6,12 +6,10 @@ var ls = {
 
     var sampler =  new ZPlaneSampler(soundSource, audioContext);
 
-    //var visualizer = new Visualizer($(".webGLContainer"));
-
   /* Load sample file over http to our buffer */
     
     var getSample = new XMLHttpRequest(); 
-    getSample.open("GET","./assets/04 No Excuses.mp3",true);
+    getSample.open("GET","./assets/01 Rotten Apple.mp3",true);
     getSample.responseType = "arraybuffer";
     getSample.onload = function(){
       var soundBuffer = audioContext.createBuffer(getSample.response,false) //flag is set for stereo
@@ -21,9 +19,12 @@ var ls = {
     };  
     getSample.send();
 
-    /* View Param Init, View GUI. Init */
+  /* View GUI. Init */
 
     ls.gui = new ls.GUI(sampler,soundSource);
+
+  /* Leap Motion control loop */
+
     leapControl(sampler, soundSource);
     
     $('body').append('<a href="http://hackreactor.com"><img style="position: fixed; top: 0; right: 0; border: 0;" src="http://i.imgur.com/x86kKmF.png" alt="Built at Hack Reactor"></a>');
