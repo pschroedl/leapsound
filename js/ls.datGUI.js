67e096310fe57acb7571628c38e38abd4e9b23c2
Peter Schroedl <peter_schroedl@me.com>
2013-09-26 15:57:27 -0700

    adds min/max sliders for each filter

--- js/ls.datGUI.js
+++ js/ls.datGUI.js
@@ -8,7 +8,14 @@ ls.datGUI = function(sample, sampler){
 
   this.loopStart = sample.loopStart;
   this.loopEnd = sample.loopEnd;
-
+  this.filter1min = sampler.filters[0].min;
+  this.filter1max = sampler.filters[0].max;
+  this.filter2min = sampler.filters[1].min;
+  this.filter2max = sampler.filters[1].max;
+  this.filter3min = sampler.filters[2].min;
+  this.filter3max = sampler.filters[2].max;
+  this.filter4min = sampler.filters[3].min;
+  this.filter4max = sampler.filters[3].max;
   document.body.appendChild( gui.domElement );
   
   var loadedSample = sample;
@@ -22,4 +29,46 @@ ls.datGUI = function(sample, sampler){
     .onChange(function(newVal){
       loadedSample.loopEnd = newVal;
   });
+
+  gui.add(this, 'filter1min').min(0).max(22000)
+    .onChange(function(newVal){
+      filter1min = newVal;
+  });
+
+  gui.add(this, 'filter1max').min(0).max(22000)
+    .onChange(function(newVal){
+      filter1max = newVal;
+  });
+
+  gui.add(this, 'filter2min').min(0).max(22000)
+    .onChange(function(newVal){
+      filter2min = newVal;
+  });
+
+  gui.add(this, 'filter2max').min(0).max(22000)
+    .onChange(function(newVal){
+      filter2max = newVal;
+  });
+
+  gui.add(this, 'filter3min').min(0).max(22000)
+    .onChange(function(newVal){
+      filter3min = newVal;
+  });
+
+  gui.add(this, 'filter3max').min(0).max(22000)
+    .onChange(function(newVal){
+      filter3max = newVal;
+  });
+
+  gui.add(this, 'filter4min').min(0).max(22000)
+    .onChange(function(newVal){
+      filter4min = newVal;
+  });
+
+  gui.add(this, 'filter4max').min(0).max(22000)
+    .onChange(function(newVal){
+      filter4max = newVal;
+  });
+
+
 };
