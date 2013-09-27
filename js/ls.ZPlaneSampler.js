6cdddc83e79bcedae1af8ff5a0eeefa27f361f9e
Peter Schroedl <peter_schroedl@me.com>
2013-09-26 20:51:12 -0700

    moves ls.Filter into own file

--- js/ls.ZPlaneSampler.js
+++ js/ls.ZPlaneSampler.js
@@ -4,52 +4,37 @@ var ZPlaneSampler = function(source, context){
   this.filterGain = [];
   this.filterLFOs = [];
 
-  var createFilterChain = function(context, soundContext, attributes){
-    // for attribute in attributes {
-    //    //context[attribute.key] = attribute.value;
-    //    //case of attribute.key being lopass, context.createbiquad + type = lopass
-    //    //Use Delegation table/router?
+  ls.FilterChain = function(context, soundContext, attributes){
 
-    //    //pass in context, soundContext,
-    //    //and list of attributes
-    //    //that reflect the webAPI specs
-
-    //    //return node - implicit in prototypal
-    // }
   };
 
   //Create filters, splitter/merger, gain and compressor
   this.filters[0] = context.createBiquadFilter();
-  this.filters[0].type = "lowpass";
+  this.filters[0].type = "bandpass";
   this.filters[0].frequency.value = 80;
-  this.filters[0].min = 40;
-  this.filters[0].max = 400;
+  this.filters[0].min = 0;
+  this.filters[0].max = 22000;
 
   this.filters[1] = context.createBiquadFilter();
-  this.filters[1].type = "lowpass";
+  this.filters[1].type = "bandpass";
   this.filters[1].frequency.value = 600;
-  this.filters[1].min = 100;
-  this.filters[1].max = 300;
+  this.filters[1].min = 0;
+  this.filters[1].max = 22000;
 
   this.filters[2] = context.createBiquadFilter();
-  this.filters[2].type = "lowpass";
+  this.filters[2].type = "bandpass";
   this.filters[2].frequency.value = 1000;
-  this.filters[2].min = 300;
-  this.filters[2].max = 1000;
+  this.filters[2].min = 0;
+  this.filters[2].max = 22000;
 
   this.filters[3] = context.createBiquadFilter();
   this.filters[3].type = "lowpass";
   this.filters[3].frequency.value = 1000;
-  this.filters[3].min = 400;
-  this.filters[3].max = 600;
+  this.filters[3].min = 0;
+  this.filters[3].max = 22000;
 
   this.splitterNode = context.createChannelSplitter(4);
   this.mergerNode = context.createChannelMerger(4);
-
-  this.outputGainNode = context.createGain();
-  this.outputGainNode.gain.value = 1;
-  
-  this.compressorNode = context.createDynamicsCompressor();
 
   //Attach filter to the splitter then back to merger
   source.connect(this.splitterNode);
@@ -72,6 +57,11 @@ var ZPlaneSampler = function(source, context){
   this.filterGain[1].connect(this.mergerNode);
   this.filterGain[2].connect(this.mergerNode);
   this.filterGain[3].connect(this.mergerNode);
+
+  this.outputGainNode = context.createGain();
+  this.outputGainNode.gain.value = .3;
+  
+  this.compressorNode = context.createDynamicsCompressor();
 
   this.mergerNode.connect(this.outputGainNode);
   this.outputGainNode.connect(context.destination);
