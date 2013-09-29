a074f22f99dad8b1e9f9a8ef69de7d2429a68d48
Peter Schroedl <peter_schroedl@me.com>
2013-09-28 17:03:46 -0700

    pares down log to simply x,y,z of leap and finger #

--- src/ls.Log.js
+++ src/ls.Log.js
@@ -1,28 +1,12 @@
-ls.Log = function(leapControlled, leapHand1, sampler){
-    console.clear();
-    console.log("Filter Frequency1 : " 
-      + leapControlled.Frequency1);
-    console.log("Filter Resonance1 : " 
-      + leapControlled.Resonance1);
-    console.log("Filter Frequency2 : " 
-      + leapControlled.Frequency2);
-    console.log("Filter Resonance2 : " 
-      + leapControlled.Resonance2);
-    console.log("LFO Rate : " 
-      + leapControlled.LFORate);
-    console.log("LFO Rate : " 
-      + leapControlled.HighPassFreq);
+ls.Log = function(leapFilters, leapHand, sampler){
+  console.clear();
 
-    console.log("(x,y,z) : "
-      + leapHand1.palmPosition[0]
-      + " " 
-      + leapHand1.palmPosition[1] 
-      + " " 
-      + leapHand1.palmPosition[2]);
-    
-    console.log("# of fingers :" + leapHand1.fingers.length);
-
-    console.log("Sampler Gain 1: " + sampler.filterGain[0].gain.value );
-    console.log("Sampler Gain 2: " + sampler.filterGain[1].gain.value );
-
-}
+  console.log("(x,y,z) : "
+    + leapHand.palmPosition[0]
+    + " " 
+    + leapHand.palmPosition[1] 
+    + " " 
+    + leapHand.palmPosition[2]);
+  
+  console.log("# of fingers :" + leapHand.fingers.length);
+};
